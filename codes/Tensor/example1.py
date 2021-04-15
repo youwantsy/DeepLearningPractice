82d727e2732bdd7180d3577b1addbf9086ee539f
youwantsy <youwantsy@naver.com>
2021-04-15 16:49:36 +0900

    2021/04/15 16:30 CNN + ignore

--- codes/Tensor/example1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-tensor = np.array(-1)
-
-print(tensor)
-print("텐서 타입: {}".format(type(tensor)))
-print("텐서 형태: {}, {}D".format(tensor.shape, tensor.ndim))
-print("텐서 요소 타입: {}".format(tensor.dtype))
